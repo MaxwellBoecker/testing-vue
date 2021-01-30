6cfad03536d9d62a76728c1413d726e2d2cf87e0
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-29 22:18:06 -0600

    (feat) logic for creating the table body out of data

---
+++ client/components/WeatherTableBody.vue
@@ -0,0 +1,22 @@
+<template>
+  <tbody>
+    <tr v-for="f in forecast" :key="f.dt_txt">
+      <td>{{f.dt_txt}}</td>
+      <td>{{f.main.temp}}</td>
+      <td>{{f.main.humidity}}%</td>
+      <td>{{f.main.pressure}}</td>
+      <td>{{f.clouds.all}}</td>
+      <td>{{f.wind.speed}}</td>
+      <td>{{f.wind.deg}}</td>
+      <td>{{f.weather[0].main}}</td>
+      <td>{{f.weather[0].description}}</td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  name: 'WeatherTableBody',
+  props: ['forecast']
+}
+</script>
