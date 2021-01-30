57cd61bd487004f1e6a7428103db1be2bd49ab21
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-29 21:01:33 -0600

    scaffolding weather table

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <div>Welcome to your Forecast Provider</div>
+      <WeatherTable :forecast="forecast"/>
+
+  </div>
 </template>
 
 <script>
+import WeatherTable from './WeatherTable.vue';
 export default {
+  components: { WeatherTable },
+  name: 'App',
+  data() {
+    return {
+      forecast: [
+        {temp: 21.3}, {temp: 21.4}
+      ]
+    };
+  },
   
+
 }
 </script>
---
+++ client/components/WeatherTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="entry in forecast" :key="entry.dt_txt">
+      {{ entry.temp }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'WeatherTable',
+  props: ['forecast']
+}
+</script>
