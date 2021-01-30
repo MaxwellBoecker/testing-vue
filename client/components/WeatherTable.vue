4f084ce3eb06764b0f78a2644d428ceb0632426c
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-29 22:17:45 -0600

    (feat) added a table to display extended forecast

--- client/components/WeatherTable.vue
+++ client/components/WeatherTable.vue
@@ -1,13 +1,28 @@
 <template>
-  <ul>
-    <li v-for="entry in forecast" :key="entry.dt_txt">
-      {{ entry.temp }}
-    </li>
-  </ul>
+  <table>
+    <thead>
+      <tr>
+        <th>Datetime</th>
+        <th>Temperature</th>
+        <th>Humidity</th>
+        <th>Pressure</th>
+        <th>Clouds</th>
+        <th>Wind speed</th>
+        <th>Wind degree</th>
+        <th>Weather</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <WeatherTableBody :forecast="forecast"/>
+  </table>
 </template>
 
 <script>
+import WeatherTableBody from './WeatherTableBody.vue';
 export default {
+  components: {
+    WeatherTableBody
+  },
   name: 'WeatherTable',
   props: ['forecast']
 }
