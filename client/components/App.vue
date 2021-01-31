2d006c885a2ee02b2225f5d82c981315eb96a1c2
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-30 20:15:39 -0600

    (cleanup) installed axios and cleaned according to eslint

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
-    <div>Welcome to your Forecast Provider</div>
+    <h1>Welcome to your Forecast Provider</h1>
       <WeatherTable :forecast="forecast"/>
   </div>
 </template>
 
 <script>
-import WeatherTable from './WeatherTable.vue';
 import axios from 'axios';
+import WeatherTable from './WeatherTable.vue';
 import config from '../.config.js';
+
 const { apikey } = config;
 export default {
   components: { WeatherTable },
   name: 'App',
   data() {
     return {
-      forecast: []
+      forecast: [],
     };
   },
   mounted() {
     const options = {
       method: 'GET',
       url: 'https://community-open-weather-map.p.rapidapi.com/forecast',
-      params: {q: 'cleveland,us', units: 'imperial'},
+      params: { q: 'cleveland,us', units: 'imperial' },
       headers: {
         'x-rapidapi-key': apikey,
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
-      }
+        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+      },
     };
 
     axios.request(options)
@@ -34,9 +35,9 @@ export default {
         // console.log(response.data.list);
         this.forecast = response.data.list;
       }).catch((error) => {
+        // display error message here
         console.error(error);
       });
-    }
-
-}
+  },
+};
 </script>
