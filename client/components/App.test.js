99a716e4028a6860997dce4e838db96ee42285ea
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-01-30 20:15:04 -0600

    Synchronous unit tests for App complete

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,19 +1,28 @@
-import { shallowMount } from '@vue/test-utils'
-import App from "./App.vue"
+/* eslint-disable no-undef */
+import { shallowMount } from '@vue/test-utils';
+import App from './App.vue';
+// import mockData from '../../mockData.json';
+
 let wrapper;
 
 beforeEach(() => {
-  const forecast = [{ temp: 21}, {temp:22}];
+  // const forecast = [{ temp: 21 }, { temp: 22 }];
+  // console.log(mockData.list.length);
   wrapper = shallowMount(App, {
-    propsData: { forecast }
+    propsData: { forecast: [] },
   });
 });
 
 afterEach(() => {
   wrapper.destroy();
 });
+
 describe('App', () => {
-  test('is a vue instance', () => {
-    expect(wrapper.isVueInstance).toBeTruthy()
-  })
-})
+  test('App is a vue instance', () => {
+    expect(wrapper.isVueInstance).toBeTruthy();
+  });
+  test('It initializes with the correct elements', () => {
+    expect(wrapper.findAll('h1').length).toEqual(1);
+    expect(wrapper.findAll('div').length).toEqual(1);
+  });
+});
