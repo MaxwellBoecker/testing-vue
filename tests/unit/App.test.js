142362f6b751770f855e4e95dda22c71ca45ff6f
MaxwellBoecker <boecker.maxwell@gmail.com>
2021-02-01 15:10:20 -0600

    added spies to check methods in App

--- tests/unit/App.test.js
+++ tests/unit/App.test.js
@@ -4,13 +4,22 @@ import App from '../../client/components/App.vue';
 // import mockData from '../../mockData.json';
 
 let wrapper;
+const spyRetrieveForecastData = jest.spyOn(App.methods, 'retrieveForecastData');
+const spyClearCity = jest.spyOn(App.methods, 'clearCity');
+const spyChangeMode = jest.spyOn(App.methods, 'changeMode');
 
 beforeEach(() => {
   // const forecast = [{ temp: 21 }, { temp: 22 }];
   // console.log(mockData.list.length);
   wrapper = shallowMount(App, {
-    propsData: { forecast: [] },
+    propsData: {
+      forecast: [],
+      mode: 'current',
+      city: '',
+      searchedCity: '',
+    },
   });
+  // spyRetrieveForecastData = jest.spyOn(App.methods, 'retrieveForecastData');
 });
 
 afterEach(() => {
@@ -23,6 +32,22 @@ describe('App', () => {
   });
   test('It initializes with the correct elements', () => {
     expect(wrapper.findAll('h1').length).toEqual(1);
-    expect(wrapper.findAll('div').length).toEqual(1);
+    expect(wrapper.findAll('div').length).toEqual(2);
+    expect(wrapper.findAll('button').length).toEqual(3);
+    expect(wrapper.findAll('h3').length).toEqual(1);
+    expect(wrapper.findAll('input').length).toEqual(1);
+  });
+  test('Mode is updated in props on button click', () => {
+    wrapper.findAll('button').at(1).trigger('click');
+    expect(wrapper.vm.mode).toMatch('5-day');
+    expect(spyChangeMode).toHaveBeenCalled()
+    wrapper.findAll('button').at(0).trigger('click');
+    expect(wrapper.vm.mode).toMatch('current');
+  });
+
+  test('methods are called on "submit" button click', () => {
+    wrapper.findAll('button').at(2).trigger('click');
+    expect(spyRetrieveForecastData).toHaveBeenCalled();
+    expect(spyClearCity).toHaveBeenCalled();
   });
 });
